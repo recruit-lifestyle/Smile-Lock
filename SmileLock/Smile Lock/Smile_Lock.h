4613d5cbe8e185afaafb250e1e625beb6363e343
Igor Kulman <igor@kulman.sk>
2018-07-18 12:02:55 +0200

    Adding a framework project for Carthage

---
+++ SmileLock/Smile Lock/Smile_Lock.h
@@ -0,0 +1,19 @@
+//
+//  Smile_Lock.h
+//  Smile Lock
+//
+//  Created by Igor Kulman on 18/07/2018.
+//  Copyright © 2018 Smile Lock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Smile_Lock.
+FOUNDATION_EXPORT double Smile_LockVersionNumber;
+
+//! Project version string for Smile_Lock.
+FOUNDATION_EXPORT const unsigned char Smile_LockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Smile_Lock/PublicHeader.h>
+
+
